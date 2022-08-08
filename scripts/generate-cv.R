5d37905ee8b2d75daf790f59b11a4c13310361cd
Daniel Sanchez <dsanchezp998@gmail.com>
2022-08-08 11:47:20 -0500

    Write

--- scripts/generate-cv.R
+++ scripts/generate-cv.R
@@ -7,5 +7,5 @@ datadrivencv::use_datadriven_cv(
   data_location = "https://docs.google.com/spreadsheets/d/1yOBU3kymAWXtOY60zPzBOmA45xm-YdYek0GDipeAVvo/edit?usp=sharing",
   pdf_location = "https://github.com/dsanchezp18/automated-cv-dsp/daniel_sanchez_cv_english.pdf",
   html_location = "https://github.com/dsanchezp18/automated-cv-dsp/daniel_sanchez_cv_english.html",
-  source_location = "https://github.com/dsanchezp18/automated-cv-dsp")
+  source_location = "https://github.com/dsanchezp18/automated-cv-dsp"
 )
